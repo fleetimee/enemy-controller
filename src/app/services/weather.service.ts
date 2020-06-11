339a0b9a7d64fea8bfc0a5c11fad2e8f6d8c12a1
Novian Andika <45744788+Novian227@users.noreply.github.com>
2020-06-11 13:08:23 +0700

    update

---
+++ src/app/services/weather.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+  url = 'https://api.openweathermap.org/data/2.5/';
+  key = 'b7e801c60cd9e8fe1bcee860567be8d7';
+  city = 'Sleman';
+  
+  constructor(private http: HttpClient) { }
+
+  getData(){
+    return this.http.get(`${this.url}weather?q=${this.city}&appid=${this.key}&units=metric`);
+  }
+
+  getForecast(){
+    return this.http.get(`${this.url}forecast?q=${this.city}&appid=${this.key}&units=metric`);
+  }
+}
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WeatherService } from '../services/weather.service';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
 
-  constructor() {}
+  public weather = {};
+  public temp = 0;
+  public city = '';
+
+  constructor(private weatherService: WeatherService) {}
+
+  ngOnInit(): void {
+    this.weatherService.getData().subscribe(result => {
+      this.weather = result;
+      this.temp = result.main.temp;
+      this.city = result.name;
+      console.log(this.weather);
+    });
+  }
 
 }
