c7baa7dfee66873c40e275ec0c8a92e7c1935efa
Novian Andika <45744788+Novian227@users.noreply.github.com>
2020-06-11 23:25:10 +0700

    add alert saat menekan favorite

--- src/app/detail/detail.page.ts
+++ src/app/detail/detail.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 
 @Component({
@@ -11,7 +12,7 @@ export class DetailPage implements OnInit {
 
   weather: any;
 
-  constructor(private route: ActivatedRoute, private router: Router) { 
+  constructor(private route: ActivatedRoute, private router: Router, private alertControllerl: AlertController) { 
     this.route.queryParams.subscribe(params => {
       if (params && params.special) {
         this.weather = JSON.parse(params.special);
@@ -22,8 +23,19 @@ export class DetailPage implements OnInit {
   ngOnInit() {
   }
 
-  save() {
+  async save() {
     localStorage.setItem('fav', JSON.stringify(this.weather));
+
+    const alert = await this.alertControllerl.create({
+      cssClass: 'my-custom-class',
+      header: 'Success !',
+      message: 'Berhasil difavoritkan <br> Silahkan cek tab Favorite',
+      buttons: ['OK']
+    });
+
+    await alert.present();
   }
+
+
 
 }
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-tab3',
+  selector: 'app-tab3', 
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
@@ -10,5 +10,4 @@ export class Tab3Page {
   constructor() {
     this.fav = JSON.parse(localStorage.getItem('fav'));
   }
-
 }
